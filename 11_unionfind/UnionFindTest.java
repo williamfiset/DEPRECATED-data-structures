4cde261498c74b07c4004b4c8f9ef2306dcd12e2
william-fiset <fisetwill@gmail.com>
2016-08-26 22:17:14 -0300

    Added tests for unionfind

--- 11_unionfind/UnionFindTest.java
+++ 11_unionfind/UnionFindTest.java
@@ -111,8 +111,52 @@ public class UnionFindTest {
     assertEquals(uf.getComponentSize(1), 5);
     assertEquals(uf.getComponentSize(2), 5);
     assertEquals(uf.getComponentSize(3), 5);
-    assertEquals(uf.getComponentSize(4), 5);         
+    assertEquals(uf.getComponentSize(4), 5);
 
+  }
+
+  @Test
+  public void testGetSize() {
+
+    UnionFind uf = new UnionFind(5);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(0,1);
+    uf.find(3);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(1,2);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(0,2);
+    uf.find(1);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(2,1);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(3,4);
+    uf.find(0);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(4,3);
+    uf.find(3);
+    assertEquals(uf.getSize(), 5);
+    uf.unify(1,3);
+    assertEquals(uf.getSize(), 5);
+    uf.find(2);
+    uf.unify(4,0);
+    assertEquals(uf.getSize(), 5);
+
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testBadUnionFindCreation1() {
+    new UnionFind(-1);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testBadUnionFindCreation2() {
+    new UnionFind(-3463);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testBadUnionFindCreation3() {
+    new UnionFind(0);
   }
 
 }

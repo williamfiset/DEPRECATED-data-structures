e5a91559a2014263aa4ab20368e7f502b2d0038e
William Fiset <fisetwill@gmail.com>
2016-12-22 19:20:44 -0400

    Worked on graph tests

--- 14_graph/GraphTest.java
+++ 14_graph/GraphTest.java
@@ -15,8 +15,14 @@ public class GraphTest {
     new GraphAdjacencyMatrix(0);
   }
 
-  public void testLegalAdjMatrixConstruction() {
+  @Test
+  public void testLegalAdjMatrixConstruction1() {
     new GraphAdjacencyMatrix(1);
+  }
+
+  @Test
+  public void testLegalAdjMatrixConstruction2() {
+    new GraphAdjacencyMatrix(10);
   }
 
   @Test(expected=IllegalArgumentException.class)
@@ -29,8 +35,43 @@ public class GraphTest {
     new GraphAdjacencyList(0);
   }
 
-  public void testLegalAdjListConstruction() {
+  @Test
+  public void testLegalAdjListConstruction1() {
     new GraphAdjacencyList(1);
+  }
+
+  @Test
+  public void testLegalAdjListConstruction2() {
+    new GraphAdjacencyList(1);
+  }
+
+  @Test
+  public void testAddingEdgesAdjMatrix() {
+    GraphAdjacencyMatrix m = new GraphAdjacencyMatrix(5);
+    m.addDirectedEdge(0, 1, 5);
+    m.addDirectedEdge(0, 1, 5);
+    m.addDirectedEdge(0, 1, 5);
+    assertEquals( 1, m.numEdges() );
+  }
+
+  @Test
+  public void testAddingEdgesAdjMatrix2() {
+    GraphAdjacencyMatrix m = new GraphAdjacencyMatrix(5);
+    m.addDirectedEdge(0, 1, 5);
+    m.addDirectedEdge(1, 4, -5);
+    m.addDirectedEdge(2, 3, 16);
+    assertEquals( 3, m.numEdges() );
+  }
+
+  @Test
+  public void testNumNodesAdjMatrix1() {
+    GraphAdjacencyMatrix m = new GraphAdjacencyMatrix(5);
+    assertEquals( 5, m.numNodes() );
+    m = new GraphAdjacencyMatrix(3);
+    assertEquals( 3, m.numNodes() );
+    m = new GraphAdjacencyMatrix(55);
+    assertEquals( 55, m.numNodes() );
+    
   }
 
 }

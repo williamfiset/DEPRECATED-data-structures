b098175c306f3ad820cbc690268c5b4c8c978aac
william-fiset <fisetwill@gmail.com>
2017-01-10 23:07:44 -0400

    Tweaked Queue code

--- 04_queue/Queue.java
+++ 04_queue/Queue.java
@@ -9,39 +9,41 @@ public class Queue <T> implements Iterable <T> {
     offer(firstElem);
   }
 
+  // Return the size of the queue
   public int size() {
     return list.size();
   }
 
+  // Returns whether or not the queue is empty
   public boolean isEmpty() {
     return size() == 0;
   }
 
+  // Peek the element at the front of the queue
+  // The method throws an error is the queue is empty
   public T peek() {
     if (isEmpty()) 
       throw new RuntimeException("Queue Empty");
     return list.peekFirst();
   }
 
+  // Poll an element from the front of the queue
+  // The method throws an error is the queue is empty
   public T poll() {
     if (isEmpty()) 
       throw new RuntimeException("Queue Empty");
     return list.removeFirst();    
   }
 
+  // Add an element to the back of the queue
   public void offer(T elem) {
     list.addLast(elem);
   }
 
+  // Return an iterator to alow the user to traverse 
+  // through the elements found inside the queue
   @Override public java.util.Iterator <T> iterator () {
-    return new java.util.Iterator <T> () {
-      @Override public boolean hasNext() {
-        return !isEmpty();
-      }
-      @Override public T next () {
-        return poll();
-      }
-    };
+    return list.iterator();
   }
 
 }

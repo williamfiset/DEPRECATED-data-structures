79f5d44556178122ca52258b3a78d29683cead27
William Fiset <fisetwill@gmail.com>
2017-04-24 19:47:55 -0300

    Bloomfilter optimization

--- BloomFilter/BloomFilter.java
+++ BloomFilter/BloomFilter.java
@@ -1,5 +1,5 @@
 /**
- * A bloom filter implementation.
+ * A generic bloom filter implementation that supports any hash function(s)
  * 
  * @author William Alexandre Fiset, william.alexandre.fiset@gmail.com
  **/
@@ -10,19 +10,22 @@ public class BloomFilter {
   // to the number of hash functions for this bloom filter
   private final int N_SETS;
 
-  // Doing '& 0x7F' is the same as modding by 64 but faster
-  private final static long MOD64 = 0x7F; // equivalently: 0b1111111;
-
   // A 2D array containing the bitsets
   private final long[][] bitsets;
 
   // Tracks the size of the bitsets in this bloom filter
   private final int[] SET_SIZES;
+
+  // Doing 'n & 0x7F' is the same as modding by 64, but faster
+  private final static long MOD64_MASK = 0x7F;
   
+  // Doing 'n >> 6' is the same as dividing by 64, but faster
+  private final static long DIV64_SHIFT = 6;
+
   // Create a bloom filter with a various bitsets of different sizes
   public BloomFilter(int[] bitSetSizes) {
-    SET_SIZES = bitSetSizes.clone();  
     N_SETS = bitSetSizes.length;
+    SET_SIZES = bitSetSizes.clone();  
     bitsets = new long[N_SETS][];
     for (int i = 0; i < N_SETS; i++) {
       bitsets[i] = new long[SET_SIZES[i]];
@@ -32,8 +35,8 @@ public class BloomFilter {
   // Add a hash value to one of the bitsets in the bloom filter
   public void add(int setIndex, long hash) {
     hash = hash % SET_SIZES[setIndex];
-    int block = (int)(hash / 64);
-    bitsets[setIndex][block] |= (1L << (hash & MOD64));
+    int block = (int)(hash >> DIV64_SHIFT);
+    bitsets[setIndex][block] |= (1L << (hash & MOD64_MASK));
   }
 
   // Adds a group of related hash values to the bloom filter.
@@ -48,8 +51,8 @@ public class BloomFilter {
   // Checks if a particular key is found within the bloom filter
   public boolean contains(long [] hashes) {
     for(int i = 0; i < hashes.length; i++) {
-      int block = (int)(hashes[i] / 64);
-      long MASK = 1L << (hashes[i] & MOD64);
+      int block = (int)(hashes[i] >> DIV64_SHIFT);
+      long MASK = 1L << (hashes[i] & MOD64_MASK);
       if ( (bitsets[i][block] & MASK) != MASK )
         return false;
     }
@@ -76,7 +79,7 @@ public class BloomFilter {
     }
 
     StringBuilder sb = new StringBuilder();
-    for (char[] row : M) sb.append(new String(row) + "\n");
+    for (char[] row : matrix) sb.append(new String(row) + "\n");
     return sb.toString();
 
   }

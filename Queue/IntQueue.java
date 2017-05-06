090106563b323e43a03e6d5828273370154af7c0
william-fiset <fisetwill@gmail.com>
2017-05-06 14:56:01 -0300

    Added fast int queue

---
+++ Queue/IntQueue.java
@@ -0,0 +1,108 @@
+/** 
+ * This files contains an implementation of an integer only queue which is very
+ * extremely quick and lightweight. In terms of performance it can outperform 
+ * java.util.ArrayDeque (Java's fastest queue implementation) by a factor of 40+! 
+ * See the benchmark test below for proof. However, the downside is you need to
+ * know an upper bound on the number of elements to put inside this queue for it to work.
+ *
+ * @author William Fiset, william.alexandre.fiset@gmail.com
+ **/
+
+public class IntQueue {
+
+  private int [] ar;
+  private int front, end, sz;
+
+  // max_sz is the maximum number of items
+  // that can be in the queue at any given time
+  public IntQueue(int max_sz) {
+    front = end = 0;
+    this.sz = max_sz+1;
+    ar = new int[sz];
+  }
+
+  public boolean isEmpty() {
+    return front == end;
+  }
+
+  // Add an element to the queue
+  public void enqueue(int value) {
+    ar[end] = value;
+    if (++end == sz) end = 0;
+    if (end == front) throw new RuntimeException("Queue too small!");
+  }
+
+  // Make sure you check is the queue is not empty before calling dequeue!
+  public int dequeue() {
+    int ret_val = ar[front];
+    if (++front == sz) front = 0;
+    return ret_val;
+  }
+
+  // Example usage
+  public static void main(String[] args) {
+    
+    IntQueue q = new IntQueue(5);
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+
+    System.out.println(q.dequeue()); // 1
+    System.out.println(q.dequeue()); // 2
+    System.out.println(q.dequeue()); // 3
+    System.out.println(q.dequeue()); // 4
+
+    System.out.println(q.isEmpty()); // false
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+
+    System.out.println(q.dequeue()); // 5
+    System.out.println(q.dequeue()); // 1
+    System.out.println(q.dequeue()); // 2
+    System.out.println(q.dequeue()); // 3
+
+    System.out.println(q.isEmpty()); // true
+
+    benchMarkTest();
+
+  }
+
+  // BenchMark IntQueue vs ArrayDeque. 
+  private static void benchMarkTest() {
+
+    int n = 10000000;
+    IntQueue iq = new IntQueue(n);
+
+    // IntQueue times at around 0.0324 seconds
+    long start = System.nanoTime();
+    for (int i = 0; i < n; i++ ) iq.enqueue(i);
+    for (int i = 0; i < n; i++ ) iq.dequeue();
+    long end = System.nanoTime();
+    System.out.println( "IntQueue Time: " +  (end-start)/1e9 );
+
+    // ArrayDeque times at around 1.438 seconds
+    java.util.ArrayDeque <Integer> adq = new java.util.ArrayDeque<>();
+    start = System.nanoTime();
+    for (int i = 0; i < n; i++ ) adq.offer(i);
+    for (int i = 0; i < n; i++ ) adq.poll();
+    end = System.nanoTime();
+    System.out.println( "ArrayDeque Time: " + (end-start)/1e9 );
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+

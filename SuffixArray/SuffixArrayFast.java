6ca3271e441dc4c6e3757398761d19431e86c226
william-fiset <fisetwill@gmail.com>
2017-05-09 17:16:27 -0300

    Patched SuffixArray

--- SuffixArray/SuffixArrayFast.java
+++ SuffixArray/SuffixArrayFast.java
@@ -8,8 +8,18 @@ public class SuffixArrayFast {
   int ALPHABET_SZ = 256, N;
   int[] T, lcp, sa, sa2, rank, tmp, c;
 
+  public SuffixArrayFast(String str) {    
+    this(toIntArray(str));    
+  }
+   
+  private static int[] toIntArray(String s) {   
+    int[] text = new int[s.length()];   
+    for(int i=0;i<s.length();i++)text[i] = s.charAt(i);   
+    return text;    
+  }
+
   // Designated constructor
-  public SuffixArray(int[] text) {
+  public SuffixArrayFast(int[] text) {
     T = text;
     N = text.length;
     sa = new int[N];

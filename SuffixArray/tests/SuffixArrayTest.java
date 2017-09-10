b133bee650bc7d95133fae4fda65ab0c8c7721c7
william-fiset <fisetwill@gmail.com>
2017-09-10 11:00:51 -0700

    Added SA tests

--- SuffixArray/tests/SuffixArrayTest.java
+++ SuffixArray/tests/SuffixArrayTest.java
@@ -15,19 +15,99 @@ public class SuffixArrayTest {
   static final int NUM_NULLS = TEST_SZ / 5;
   static final int MAX_RAND_NUM = 250;
 
+  String ASCII_LETTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
   @Before
   public void setup() { }
 
   @Test
   public void suffixArrayLength() {
     
-    SuffixArray sa1 = new SuffixArraySlow("ABCD");
-    SuffixArray sa2 = new SuffixArrayMed("ABCD");
-    SuffixArray sa3 = new SuffixArrayFast("ABCD");
+    String str = "ABCDE";
     
-    assertEquals(sa1.N, 4);
-    assertEquals(sa2.N, 4);
-    assertEquals(sa3.N, 4);
+    SuffixArray sa1 = new SuffixArraySlow(str);
+    SuffixArray sa2 = new SuffixArrayMed(str);
+    SuffixArray sa3 = new SuffixArrayFast(str);
+    
+    assertEquals(str.length(), sa1.N);
+    assertEquals(str.length(), sa2.N);
+    assertEquals(str.length(), sa3.N);
+    
+  }
+
+  @Test
+  public void lcsUniqueCharacters() {
+    
+    SuffixArray sa1 = new SuffixArraySlow(ASCII_LETTERS);
+    SuffixArray sa2 =  new SuffixArrayMed(ASCII_LETTERS);
+    SuffixArray sa3 = new SuffixArrayFast(ASCII_LETTERS);
+    
+    SuffixArray[] suffixArrays = {sa1, sa2, sa3};
+
+    for (SuffixArray sa : suffixArrays) {
+      for (int i = 0; i < sa.N; i++) {
+        assertEquals(0, sa.lcp[i]);
+      }
+    }
+    
+  }
+
+  @Test
+  public void increasingLCPTest() {
+    
+    String UNIQUE_CHARS = "KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK";
+    
+    SuffixArray sa1 = new SuffixArraySlow(UNIQUE_CHARS);
+    SuffixArray sa2 =  new SuffixArrayMed(UNIQUE_CHARS);
+    SuffixArray sa3 = new SuffixArrayFast(UNIQUE_CHARS);
+    
+    SuffixArray[] suffixArrays = {sa1, sa2, sa3};
+
+    for (SuffixArray sa : suffixArrays) {
+      for (int i = 0; i < sa.N; i++) {
+        assertEquals(i, sa.lcp[i]);
+      }
+    }
+    
+  }
+
+  @Test
+  public void lcpTest1() {
+    
+    String text = "ABBABAABAA";
+    int[] lcpValues = {0,1,2,1,4,2,0,3,2,1};
+
+    SuffixArray sa1 = new SuffixArraySlow(text);
+    SuffixArray sa2 =  new SuffixArrayMed(text);
+    SuffixArray sa3 = new SuffixArrayFast(text);
+    
+    SuffixArray[] suffixArrays = {sa1, sa2, sa3};
+
+    for (SuffixArray sa : suffixArrays) {
+      for (int i = 0; i < sa.N; i++) {
+        assertEquals(lcpValues[i], sa.lcp[i]);
+      }
+    }
+    
+  }
+
+  @Test
+  public void lcpTest2() {
+    
+    String text = "ABABABAABB";
+    int[] lcpValues = {0,1,3,5,2,0,1,2,4,1};
+
+    SuffixArray sa1 = new SuffixArraySlow(text);
+    SuffixArray sa2 =  new SuffixArrayMed(text);
+    SuffixArray sa3 = new SuffixArrayFast(text);
+    
+    SuffixArray[] suffixArrays = {sa1, sa2, sa3};
+
+    for (SuffixArray sa : suffixArrays) {
+      for (int i = 0; i < sa.N; i++) {
+        assertEquals(lcpValues[i], sa.lcp[i]);
+      }
+    }
     
   }
 

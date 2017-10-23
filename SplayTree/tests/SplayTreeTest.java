896047dd951f6c9a438ea25b12f3a4d8faa709d0
William Fiset <fisetwill@gmail.com>
2017-10-22 18:10:34 -0700

    Added tests

--- SplayTree/tests/SplayTreeTest.java
+++ SplayTree/tests/SplayTreeTest.java
@@ -17,13 +17,22 @@ public class SplayTreeTest {
   }
 
   @Test
+  public void testContainsEmptyTree() {
+    assertFalse(tree.contains(6));
+    assertFalse(tree.contains(-5));
+    assertFalse(tree.contains(0));
+    assertFalse(tree.contains(123));
+    assertFalse(tree.contains(99));
+  }
+
+  @Test
   public void testInsertionMethod() {
     tree.insert(43);
     assertTrue(tree.contains(43));
   }
 
   @Test
-  public void testInsertionMethod() {
+  public void testInsertionMethod2() {
     tree.insert(1);
     tree.insert(2);
     tree.insert(3);

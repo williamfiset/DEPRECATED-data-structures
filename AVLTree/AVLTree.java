6e07ffc4242fb81ab005df66ca675c8e2e57156b
william-fiset <fisetwill@gmail.com>
2017-10-28 15:31:46 -0700

    AVL Tree insertion working

--- AVLTree/AVLTree.java
+++ AVLTree/AVLTree.java
@@ -1,37 +1,71 @@
 
-
 public class AVLTree <T extends Comparable<T>> {
 
-  static class Node <T extends Comparable<T>> {
+  static class Node <T extends Comparable<T>> implements TreePrinter.PrintableNode {
     // Balance factor
     int bf;
     T value;
     int height;
     Node<T> left, right;
     public Node(T value, Node<T> left, Node<T> right) {
-      if (value == null) throw new IllegalArgumentException();
       this.value = value;
       this.left = left;
       this.right = right;
-      this.height = -1;
+    }
+    @Override 
+    public Node getLeft() {
+      return left;
+    }
+    @Override
+    public Node getRight() {
+      return right;
+    }
+    @Override
+    public String getText() {
+      return value+""; // + "("+bf+","+height+")";
     }
   }
 
   Node<T> root;
+  int size = 0;
+
+  public boolean contains(T value) {
+    return contains(root, value);
+  }
+
+  private boolean contains(Node<T> node, T value) {
+    
+    if (node == null) return false;
+
+    // Compare current value to the value in the node.
+    int cmp = value.compareTo(node.value);
+
+    // Dig into left subtree.
+    if (cmp < 0) return contains(node.left, value);
+
+    // Dig into right subtree.
+    if (cmp > 0) return contains(node.right, value);
+
+    // Found value in tree.
+    return true;
+
+  }
 
   public void insert(T value) {
-    root = insert(root, value);
+    if (value == null) throw new IllegalArgumentException();
+    if (!contains(value)) {
+      root = insert(root, value);
+      size++;
+    }
   }
 
   private Node<T> insert(Node<T> node, T value) {
     
     // Base case.
-    if (node == null) {
-      return new Node<T>(value, null, null);
-    }
+    if (node == null) return new Node<T>(value, null, null);
 
     // Compare current value to the value in the node.
-    int cmp = node.value.compareTo(value);
+    int cmp = value.compareTo(node.value);
 
     // Dig into left subtree.
     if (cmp < 0) {
@@ -42,14 +76,11 @@ public class AVLTree <T extends Comparable<T>> {
       node.right = insert(node.right, value);
 
     // Duplicate value in tree.
-    } else {
-      System.out.println("Duplicate value :0");
-      return node;
-    }
+    } else return node;
 
     update(node);
 
-    // Rebalance tree.
+    // Re-balance tree.
     return balance(node);
 
   }
@@ -77,7 +108,7 @@ public class AVLTree <T extends Comparable<T>> {
     if (node.bf == -2) {
 
       // Left-Left case.
-      if (node.left.br <= 0) {
+      if (node.left.bf <= 0) {
         return leftLeftCase(node);
         
       // Left-Right case.
@@ -90,11 +121,11 @@ public class AVLTree <T extends Comparable<T>> {
 
       // Right-Right case.
       if (node.right.bf >= 0) {
-        return rightLeftCase(node);
+        return rightRightCase(node);
 
       // Right-Left case.
       } else {
-        return rightRightCase(node);
+        return rightLeftCase(node);
       }
 
     }
@@ -140,7 +171,52 @@ public class AVLTree <T extends Comparable<T>> {
     return newParent;
   }
 
+  // The height of a rooted tree is the number of edges between the tree's
+  // root and its furthest leaf. This means that a tree containing a single 
+  // node has a height of 0.
+  public int height() {
+    if (root == null) return 0;
+    return root.height;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public void display() {
+    TreePrinter.print(root);
+  }
+
+  public static void main(String[] args) {
+    AVLTree<Integer> tree = new AVLTree<>();
+    tree.insert(1);    
+    tree.insert(2);    
+    tree.insert(3);    
+    tree.insert(44);    
+    tree.insert(-4);    
+    tree.insert(6);    
+    tree.insert(-8);    
+    tree.insert(-12);    
+    tree.insert(-8);    
+    tree.insert(-9);    
+    tree.insert(-10);    
+    tree.insert(300);    
+    tree.insert(43);    
+    tree.insert(9);    
+    tree.insert(43);    
+    tree.display();
+  }
+
 }
+
+
+
+
+
 
 
 
---
+++ AVLTree/tests/AVLTreeTest.java
@@ -0,0 +1,161 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.*;
+
+public class AVLTreeTest {
+
+  static final int MAX_RAND_NUM = +100000;
+  static final int MIN_RAND_NUM = -100000;
+
+  static final int TEST_SZ = 10000;
+
+  private AVLTree<Integer> tree;
+
+  @Before
+  public void setup() {
+    tree = new AVLTree<>();
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void testNullInsertion() {
+    tree.insert(null);
+  }
+
+  @Test
+  public void testTreeContainsNull() {
+    assertFalse(tree.contains(null));
+  }
+
+  @Test
+  public void testLeftLeftCase() {
+    
+    tree.insert(3);
+    tree.insert(2);
+    tree.insert(1);
+
+    assertEquals(2, tree.root.value.intValue());
+    assertEquals(1, tree.root.left.value.intValue());
+    assertEquals(3, tree.root.right.value.intValue());
+
+    assertNull(tree.root.left.left);
+    assertNull(tree.root.left.right);
+    assertNull(tree.root.right.left);
+    assertNull(tree.root.right.right);
+
+  }
+
+  @Test
+  public void testLeftRightCase() {
+
+    tree.insert(3);
+    tree.insert(1);
+    tree.insert(2);
+
+    assertEquals(2, tree.root.value.intValue());
+    assertEquals(1, tree.root.left.value.intValue());
+    assertEquals(3, tree.root.right.value.intValue());
+
+    assertNull(tree.root.left.left);
+    assertNull(tree.root.left.right);
+    assertNull(tree.root.right.left);
+    assertNull(tree.root.right.right);
+    
+  }
+
+  @Test
+  public void testRightRightCase() {
+
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(3);
+
+    assertEquals(2, tree.root.value.intValue());
+    assertEquals(1, tree.root.left.value.intValue());
+    assertEquals(3, tree.root.right.value.intValue());
+
+    assertNull(tree.root.left.left);
+    assertNull(tree.root.left.right);
+    assertNull(tree.root.right.left);
+    assertNull(tree.root.right.right);
+    
+  }
+
+  @Test
+  public void testRightLeftCase() {
+
+    tree.insert(1);
+    tree.insert(3);
+    tree.insert(2);
+
+    assertEquals(2, tree.root.value.intValue());
+    assertEquals(1, tree.root.left.value.intValue());
+    assertEquals(3, tree.root.right.value.intValue());
+
+    assertNull(tree.root.left.left);
+    assertNull(tree.root.left.right);
+    assertNull(tree.root.right.left);
+    assertNull(tree.root.right.right);
+    
+  }
+
+  @Test
+  public void testRandomizedValueInsertionsAgainstTreeSet() {
+
+    TreeSet<Integer> set = new TreeSet<>();
+    for (int i = 0; i < TEST_SZ; i++) {
+      int v = randValue();
+      set.add(v);
+      tree.insert(v);
+      assertEquals(set.size(), tree.size());
+      assertTrue(validateBSTInvarient(tree.root));
+    }
+
+  }
+
+  static <T extends Comparable<T>> boolean validateBSTInvarient(AVLTree.Node<T> node) {
+    if (node == null) return true;
+    T val = node.value;
+    boolean v = true;
+    if (node.left != null)  v = v && node.left.value.compareTo(val)  < 0;
+    if (node.right != null) v = v && node.right.value.compareTo(val) > 0;
+    return v && validateBSTInvarient(node.left) && validateBSTInvarient(node.right);
+  }
+
+  @Test
+  public void testTreeHeight() {
+    for (int n = 1; n <= TEST_SZ; n++) {
+      
+      tree.insert(randValue());
+      int height = tree.height();
+
+      // Get an upper bound on what the maximum height of
+      // an AVL tree should be. Values were taken from:
+      // https://en.wikipedia.org/wiki/AVL_tree#Comparison_to_other_structures
+      double c = 1.441;
+      double b = -0.329;
+      double upperBound = c*(Math.log(n+2.0)/Math.log(2)) + b;
+
+      assertTrue(height < upperBound);
+
+    }
+  }
+
+  public static int randValue() {
+    return (int)(Math.random() * MAX_RAND_NUM*2) + MIN_RAND_NUM;    
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
